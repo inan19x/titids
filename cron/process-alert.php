e5feff5d7ade4be901e54d116e3100f0da4b3573
Ade Ismail Isnan <61326551+inan19x@users.noreply.github.com>
2020-04-24 11:24:36 +0700

    Update process-alert.php

--- cron/process-alert.php
+++ cron/process-alert.php
@@ -1,30 +1,37 @@
 <?php
-//
+// ==========================================================================================================
 // titids - Tiny Thick IDS v0.0.1 by Ade Ismail Isnan
 // Copyleft 2010
 //
 // Execute this script from root every minute/hour/day via cronjob to process Suricata's fast.log to Database
 // eg.: # crontab -e
-//
+// and run cron job every minute: * * * * * /usr/bin/php -q /path/to/titids/cron/process-alert.php
+// ==========================================================================================================
 
+// ===================================================
+//
 // Suricata fast.log location:
 $fastlog = "/var/log/suricata/fast.log";
+// MySQL database connection config:
+$filedbcon = "/var/www/html/titids/mysql_connect.php";
+//
+// ===================================================
 
-include "mysql_connect.php";
+include "$filedbcon";
 $file = fopen($fastlog,"r+");
 
 while (!feof($file)) {
-	$content = fgets($file);
-	$re = "/^\d+\/\d+\/\d+-\d+:\d+:\d+.\d+\s.\[\*\*\]\s+\[\d+:\d+:\d+\]\s+(?P<alert>\w.+)\s+\[\**]\s+\[Classification:\s+\S.+\]\s+\[Priority:\s+\d+\]\s+\{(?P<proto>\S+)\}\s+(?P<intruder>\d+.\d+.\d+.\d+):\d+\s+\-\>\s+\d+.\d+.\d+.\d+:(?P<port>\d+)/";
-	preg_match_all($re, $content, $my_array, PREG_SET_ORDER,0);
-	$alert=$my_array[0][1];
-	$proto=$my_array[0][2];
-	$ip=$my_array[0][3];
-	$port=$my_array[0][4];
-	if(!empty($ip)){
-		$sql="insert into INTRUDERS(Alert,IP,Protocol,Port) values ('$alert','$ip','$proto','$port');";
-		mysql_query($sql);
-	}
+        $content = fgets($file);
+        $re = "/^\d+\/\d+\/\d+-\d+:\d+:\d+.\d+\s.\[\*\*\]\s+\[\d+:\d+:\d+\]\s+(?P<alert>\w.+)\s+\[\**]\s+\[Classification:\s+\S.+\]\s+\[Priority:\s+\d+\]\s+\{(?P<proto>\S+)\}\s+(?P<intruder>\d+.\d+.\d+.\d+):\d+\s+\-\>\s+\d+.\d+.\d+.\d+:(?P<port>\d+)/";
+        preg_match_all($re, $content, $my_array, PREG_SET_ORDER,0);
+        $alert=$my_array[0][1];
+        $proto=$my_array[0][2];
+        $ip=$my_array[0][3];
+        $port=$my_array[0][4];
+        if(!empty($ip)){
+                $sql="insert into INTRUDERS(Alert,IP,Protocol,Port) values ('$alert','$ip','$proto','$port');";
+                mysql_query($sql);
+        }
 }
 ftruncate($file,0);
 fclose($file)
