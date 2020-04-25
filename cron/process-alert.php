a70f7e57f0b09b3ee75ab215d894089a7e180b8c
Ade Ismail Isnan <61326551+inan19x@users.noreply.github.com>
2020-04-25 21:14:33 +0700

    Update process-alert.php

--- cron/process-alert.php
+++ cron/process-alert.php
@@ -1,7 +1,7 @@
 <?php
 // ==========================================================================================================
 // titids - Tiny Thick IDS v0.0.1 by Ade Ismail Isnan
-// Copyleft 2010
+// Snort/Suricata alert log parser - Copyleft 2010
 //
 // Execute this script from root every minute/hour/day via cronjob to process Suricata's fast.log to Database
 // eg.: # crontab -e
@@ -13,7 +13,7 @@
 // Suricata fast.log location:
 $fastlog = "/var/log/suricata/fast.log";
 // MySQL database connection config:
-$filedbcon = "/var/www/html/titids/mysql_connect.php";
+$filedbcon = "/path/to/titids/cron/mysql_connect.php";
 //
 // ===================================================
 
@@ -22,14 +22,19 @@ $file = fopen($fastlog,"r+");
 
 while (!feof($file)) {
         $content = fgets($file);
-        $re = "/^\d+\/\d+\/\d+-\d+:\d+:\d+.\d+\s.\[\*\*\]\s+\[\d+:\d+:\d+\]\s+(?P<alert>\w.+)\s+\[\**]\s+\[Classification:\s+\S.+\]\s+\[Priority:\s+\d+\]\s+\{(?P<proto>\S+)\}\s+(?P<intruder>\d+.\d+.\d+.\d+):\d+\s+\-\>\s+\d+.\d+.\d+.\d+:(?P<port>\d+)/";
+        $re = "/^(?P<month>\d+)\/(?P<day>\d+)\/(?P<year>\d+)-(?P<times>\d+:\d+:\d+).\d+\s.\[\*\*\]\s+\[\d+:\d+:\d+\]\s+(?P<alert>\w.+)\s+\[\**]\s+\[Classification:\s+\S.+\]\s+\[Priority:\s+\d+\]\s+\{(?P<proto>\S+)\}\s+(?P<intruder>\d+.\d+.\d+.\d+):\d+\s+\-\>\s+\d+.\d+.\d+.\d+:(?P<port>\d+)/";
         preg_match_all($re, $content, $my_array, PREG_SET_ORDER,0);
-        $alert=$my_array[0][1];
-        $proto=$my_array[0][2];
-        $ip=$my_array[0][3];
-        $port=$my_array[0][4];
+        $m=$my_array[0][1];
+        $d=$my_array[0][2];
+        $Y=$my_array[0][3];
+        $t=$my_array[0][4];
+        $alerttime="$Y-$m-$d $t";
+        $alert=$my_array[0][5];
+        $proto=$my_array[0][6];
+        $ip=$my_array[0][7];
+        $port=$my_array[0][8];
         if(!empty($ip)){
-                $sql="insert into INTRUDERS(Alert,IP,Protocol,Port) values ('$alert','$ip','$proto','$port');";
+                $sql="insert into INTRUDERS(Time,Alert,IP,Protocol,Port) values ('$alerttime','$alert','$ip','$proto','$port');";
                 mysql_query($sql);
         }
 }
